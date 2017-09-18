fa594a728b8b5a8e5e6f5bb4f9f3658e1984f237
incuisting <xuyanting9634@163.com>
2017-09-18 19:41:09 +0800

    postcss

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                },
 	      {
                test: /\.scss$/,
-               loader: ['style-loader', 'css-loader','sass-loader'],
+               loader: ['style-loader', 'css-loader','postcss-loader','sass-loader'],
                include: path.resolve(__dirname, 'src')
               }
           ]
